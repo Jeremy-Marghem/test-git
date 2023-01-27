aa8ff3d3c6ba8e09841db243f1a57f8519a428ac
Jeremy Marghem <jma@bizzdev.net>
2023-01-27 16:37:12 +0100

    Creating console application

---
+++ Console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+        }
+    }
+}
